c7206658d7a618c5e10fb14f51e0b2c5987ac9fe
huy.cuong <cuong.dao@inkr.com>
2022-03-28 15:32:40 +0700

    Add HueSlider

--- packages/ColorGradientPicker.tsx
+++ packages/ColorGradientPicker.tsx
@@ -1,25 +1,65 @@
 import cn from "clsx";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 import s from "./ColorGradientPicker.module.css";
+import HueSlider from "./components/HueSlider/HueSlider";
 import Input from "./components/Input";
 import { DEFAULT_CLASS_NAME } from "./constants";
 import { ColorGradientPickerProps } from "./types";
+import { Hsv } from "./utils/colorTypes";
+import hexToHsv from "./utils/hexToHsv";
+import hsvToHex from "./utils/hsvToHex";
+import sanitizeHex from "./utils/sanitizeHex";
 
 function ColorGradientPicker(props: ColorGradientPickerProps) {
   // ------------------------------------------------------------------------------------------
   const { classNamePrefix = DEFAULT_CLASS_NAME, className } = props;
 
   // ------------------------------------------------------------------------------------------
-  const [isActive, setActive] = useState<boolean>(false);
+
+  // const sanitizedColor = sanitizeHex(color || "#000000");
+  const sanitizedColor = sanitizeHex("#000000");
+
+  const [hex, setHex] = useState<string>(sanitizedColor);
+  const [hsv, setHsv] = useState(hexToHsv(sanitizedColor));
+
+  const [isOpenPicker, setOpenPicker] = useState<boolean>(false);
+
+  const hsvRef = useRef(hsv);
+  const hexRef = useRef(hex);
+
+  // ------------------------------------------------------------------------------------------
 
   const onShowPanel = useCallback(() => {
-    setActive(true);
+    setOpenPicker(true);
   }, []);
 
   const onHidePanel = useCallback(() => {
-    setActive(false);
-  }, [])
+    setOpenPicker(false);
+  }, []);
+
+  // Set the hex and hsv states/refs with updated data
+  const setColor = (updatedHex: string, updatedHsv: Hsv) => {
+    hexRef.current = updatedHex;
+
+    setHex(updatedHex);
+    setHsv(updatedHsv);
+
+    // onChange(updatedHex);
+  };
+
+  // Helper to set the color when HSV change
+  const setColorFromHsv = (updatedHsv: Hsv) =>
+    setColor(
+      hsvToHex(updatedHsv.hue, updatedHsv.saturation, updatedHsv.value),
+      updatedHsv,
+    );
+
+  const {
+    hue,
+    // saturation,
+    //  value
+  } = hsv;
 
   return (
     <div className={cn(s.wrapper, classNamePrefix, className)}>
@@ -33,8 +73,18 @@ function ColorGradientPicker(props: ColorGradientPickerProps) {
         onExtraInputBlur={onHidePanel}
       />
 
-      {isActive && (
-        <div>Color panel</div>
+      {isOpenPicker && (
+        <div>
+          <HueSlider
+            hue={hue}
+            onChange={(updatedHue) =>
+              setColorFromHsv({
+                ...hsvRef.current,
+                hue: updatedHue,
+              })
+            }
+          />
+        </div>
       )}
     </div>
   );
---
+++ packages/components/HueSlider/HueSlider.tsx
@@ -0,0 +1,118 @@
+import clsx from "clsx";
+import React, { useCallback, useRef, useState } from "react";
+
+import { getHueFromPosition } from "../../utils/common";
+import hsvToHex from "../../utils/hsvToHex";
+import s from "./HueSlider.module.css";
+
+type HueSliderProps = {
+  hue: number;
+  onChange: (hue: number) => void;
+};
+
+const HueSlider = ({
+  hue,
+  onChange,
+}: HueSliderProps) => {
+  const [isInteracting, setIsInteracting] = useState(false);
+  const sliderDivRef = useRef<HTMLDivElement>(null);
+
+  const hueColor = hsvToHex(hue, 1, 1);
+
+  const updateHue = useCallback(
+    (
+      evt: React.MouseEvent<Element, MouseEvent> | React.PointerEvent<Element>,
+    ) => {
+      if (!sliderDivRef.current) {
+        return;
+      }
+
+      const huePosition = sliderDivRef.current.getBoundingClientRect();
+
+      const x = evt.clientX - huePosition.left;
+      const updatedHue = getHueFromPosition(
+        x,
+        sliderDivRef.current.clientWidth,
+      );
+      onChange(updatedHue);
+    },
+    [onChange],
+  );
+
+  const onPointerDown = useCallback(
+    (evt: React.PointerEvent<Element>): void => {
+      (evt.target as HTMLElement).setPointerCapture(evt.pointerId);
+      setIsInteracting(true);
+      updateHue(evt);
+    },
+    [updateHue],
+  );
+
+  const onPointerUp = useCallback(
+    (evt: React.PointerEvent<Element>): void => {
+      (evt.target as HTMLElement).releasePointerCapture(evt.pointerId);
+      setIsInteracting(false);
+    },
+    [],
+  );
+
+  const onMouseDown = useCallback(
+    (evt: React.MouseEvent<Element, MouseEvent>): void => {
+      setIsInteracting(true);
+      updateHue(evt);
+    },
+    [updateHue],
+  );
+
+  const onMove = useCallback(
+    (
+      evt: React.MouseEvent<Element, MouseEvent> | React.PointerEvent<Element>,
+    ): void => {
+      if (isInteracting) {
+        updateHue(evt);
+      }
+    },
+    [isInteracting, updateHue],
+  );
+
+  const onMouseUp = useCallback(() => {
+    setIsInteracting(false);
+  }, []);
+
+  // Setup pointer events for supported browsers for two reasons:
+  //   1. It allows for pointer capture which allows for continued
+  //      interaction even when the cursor/pointer outside of picker
+  //   2. It allows for unified code across devices (mobile and desktop)
+  const interactionCallbacks = window.PointerEvent
+    ? {
+        onPointerDown,
+        onPointerMove: onMove,
+        onPointerUp,
+      }
+    : {
+        onMouseDown,
+        onMouseMove: onMove,
+        onMouseUp,
+      };
+
+  const sliderStyle = {
+    left: `${(hue / 360) * 100}%`,
+    backgroundColor: hueColor,
+  };
+
+  return (
+    <div
+      className={clsx(s.hue_slider)}
+      ref={sliderDivRef}
+      title="Hue"
+      {...interactionCallbacks}
+    >
+      <div
+        className={clsx(s.hue_slider_picker)}
+        style={sliderStyle}
+      />
+    </div>
+  );
+};
+
+export default HueSlider;
