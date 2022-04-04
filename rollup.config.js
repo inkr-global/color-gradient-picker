69b965c4f30ee5242c72ccc4b33131a38d0269bc
huycuongdao <daohuycuong92@gmail.com>
2022-04-04 17:38:42 +0700

    Add rollup build

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import typescript from "rollup-plugin-typescript2";
+
+const packageJson = require("./package.json");
+
+export default {
+  input: "packages/index.ts",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: false,
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: false,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss({
+      extensions: [".css"],
+    }),
+  ],
+};
