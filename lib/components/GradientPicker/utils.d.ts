16c493904acf632d7b4984d90aab6e1b00e9eaab
huycuongdao <daohuycuong92@gmail.com>
2022-04-04 23:33:18 +0700

    Rebuild

--- lib/components/GradientPicker/utils.d.ts
+++ lib/components/GradientPicker/utils.d.ts
@@ -1,4 +1,4 @@
 import { Gradient, Points } from "../../colorTypes";
-export declare const sortPalette: (palette: Points[]) => Points[];
+export declare const sortPoints: (points: Points[]) => Points[];
 export declare const getLinearGradientBackgroundCss: (gradient: Gradient) => string;
 export declare const noop: () => undefined;
