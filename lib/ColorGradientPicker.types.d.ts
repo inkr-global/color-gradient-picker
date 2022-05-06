eae245f9e228826f2b2fea6512039ba0a37123b3
huy.cuong <daohuycuong92@gmail.com>
2022-05-06 22:03:03 +0700

    Update build

--- lib/ColorGradientPicker.types.d.ts
+++ lib/ColorGradientPicker.types.d.ts
@@ -1,6 +1,7 @@
 import { Alpha, Gradient, Hex } from "./colorTypes";
 import { InputProps } from "./components/Input/types";
 export declare type ColorType = "linear-gradient" | "solid";
+export declare type PickerPlacement = "top-right" | "top-left" | "bottom-right" | "bottom-left";
 export interface ColorValue {
     alpha?: Alpha;
     solid?: Hex;
@@ -10,6 +11,7 @@ export interface ColorValue {
 export interface ColorGradientPickerProps extends Omit<InputProps, "onChange"> {
     color: ColorValue;
     onChange: (color: ColorValue) => void;
+    pickerPlacement?: PickerPlacement;
     colorSelectType?: ColorType | "all";
     colorPickingPanelClassName?: string;
     hasAlphaInput?: boolean;
